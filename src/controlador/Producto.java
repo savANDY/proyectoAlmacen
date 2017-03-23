16479e6c96d6e317faad2d390b8d18cc8b508068
savANDY <andicuu@gmail.com>
2017-03-23 21:03:30 +0100

    Clase controlador.Producto creada

---
+++ src/controlador/Producto.java
@@ -0,0 +1,68 @@
+package controlador;
+
+public class Producto {
+
+	private int id;
+	private String nombre;
+	private String proveedor;
+	private double precio;
+	private int existencias;
+
+	// Getters y Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(String proveedor) {
+		this.proveedor = proveedor;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getExistencias() {
+		return existencias;
+	}
+
+	public void setExistencias(int existencias) {
+		this.existencias = existencias;
+	}
+
+	// Constructor
+	public Producto(int id, String nombre, String proveedor, double precio, int existencias) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.proveedor = proveedor;
+		this.precio = precio;
+		this.existencias = existencias;
+	}
+
+	// Constructor vacio
+
+	public Producto() {
+		super();
+	}
+
+}
